53399ce83b0b46ebdce03d9385711ea8b2fed459
Angela Branaes <angela.branaes@gmail.com>
2017-06-22 16:53:32 +0100

    Added python setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='FullStackTemplate',
+    version='1.0.0',
+    description='A Template for creating a Full Stack Web Application using Python, NPM, Webpack and React',
+    author='Angela Branaes',
+    author_email='angela.branaes10@alumni.imperial.ac.uk',
+    license='MIT',
+    url='https://github.com/angineering/FullStackTemplate',
+    keywords = [ 'fullstack', 'template', 'python', 'react','npm', 'webpack'],
+    classifiers = [
+    'Topic :: Software Development',
+    'Natural Language :: English'
+    ],
+)
