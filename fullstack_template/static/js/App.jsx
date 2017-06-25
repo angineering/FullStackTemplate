0871cd0c43c7ef4dbaccb308348e587bb893543a
Angela Branaes <angela.branaes@gmail.com>
2017-06-26 00:03:10 +0100

    Added background image for header

--- fullstack_template/static/js/App.jsx
+++ fullstack_template/static/js/App.jsx
@@ -3,18 +3,24 @@ import Hello from "./Hello";
 import { PageHeader } from "react-bootstrap";
 
 require('../css/fullstack.css');
-
 var $ = require('jquery');
+
+import HeaderBackgroundImage from '../images/header.jpg';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
+	}
+	addHeaderImg() {
+		let headerBg = new Image();
+		headerBg.src = HeaderBackgroundImage;
 	}
 
 	render () {
         return (
 			<PageHeader>
 				<div className='header-contents'>
+				{this.addHeaderImg()}
 				<Hello name='Rimini' />
 				</div>
 			</PageHeader>
