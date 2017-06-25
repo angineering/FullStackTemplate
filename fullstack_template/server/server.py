59005ad339b7e4569d514a20a17655e64a29f351
Angela Branaes <angela.branaes@gmail.com>
2017-06-25 22:08:25 +0100

    Changed python quotes to single quotes

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/hello") # take note of this decorator syntax, it's a common pattern
+@app.route('/hello') # take note of this decorator syntax, it's a common pattern
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
