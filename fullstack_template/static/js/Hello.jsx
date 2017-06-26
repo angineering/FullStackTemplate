ef7b493b4e6242a63370833659a57301434488e4
Angela Branaes <angela.branaes@gmail.com>
2017-06-26 21:07:02 +0100

    Updated Hello.jsx to say greeting + props.name

--- fullstack_template/static/js/Hello.jsx
+++ fullstack_template/static/js/Hello.jsx
@@ -6,25 +6,30 @@ var $ = require('jquery');
 export default class Hello extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {greeting: "Hello " + this.props.name};
+		this.state = {greeting: 'Hello ' + this.props.name};
 
 		// This binding is necessary to make `this` work in the callback
 		this.getPythonHello = this.getPythonHello.bind(this);
 	}
 
+	personaliseGreeting(greeting) {
+		this.setState({greeting: greeting + ' ' + this.props.name + '!'});
+	}
+
 	getPythonHello() {
 		$.get(window.location.href + 'hello', (data) => {
 			console.log(data);
-			this.setState({greeting: data});
+			this.personaliseGreeting(data);
 		});
 	}
-	
+
 	render () {
         return (
 			<div>
 			<h1>{this.state.greeting}</h1>
-			<Button bsSize="large" bsStyle="primary" onClick={this.getPythonHello}>
-			Click for a new Greeting!
+			<hr/>
+			<Button bsSize="large" bsStyle="danger" onClick={this.getPythonHello}>
+			Say Hello!
 			</Button>
 			</div>
 		);
