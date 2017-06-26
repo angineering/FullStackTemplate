38cddd9cbf3e2b4dd6376065e29c2ca636b16a57
Angela Branaes <angela.branaes@gmail.com>
2017-06-26 21:04:05 +0100

    The hello endpoint now returns a random European greeting

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
@@ -9,7 +10,15 @@ def index():
 
 @app.route('/hello') # take note of this decorator syntax, it's a common pattern
 def hello():
-    return 'Hello World!'
+    # It is good practice to only call a function in your route end-point,
+    # rather than have actual implementation code here.
+    # This allows for easier unit and integration testing of your functions.
+    return get_hello()
+
+
+def get_hello():
+    greeting_list = ['Bonjour', 'Ciao', 'Hei', 'Salut', 'Hola', 'Buongiorno']
+    return random.choice(greeting_list)
 
 
 if __name__ == '__main__':
